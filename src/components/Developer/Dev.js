1b1f1465fb335ee1b662ad8d3f9c3c679800d1ca
JSLinn727 <justinslinn727@gmail.com>
2017-08-30 12:00:04 -0400

    More mobile fixed

--- src/components/Developer/Dev.js
+++ src/components/Developer/Dev.js
@@ -7,17 +7,15 @@ class Dev extends Component {
   render() {
     return (
       <div className="card" id="devCard">
-        <img className="headshot" src={headshot} alt="Picture of the Developer" />
-        <div className="dev">
-          <div className="aside">
-            <h3>About The Developer</h3>
-            <p>
-              Dolor laboris tempor ullamco nostrud proident aliquip sit cupidatat dolor. Ullamco nulla aliqua nostrud do
-              qui laborum adipisicing exercitation id cillum tempor dolore amet. In aliqua ad nisi do est in minim anim
-              ex dolore aliqua ipsum. Nulla adipisicing
-            </p>
-            <Contact />
-          </div>
+        <img className="headshot" id="headshot" src={headshot} alt="Picture of the Developer" />
+        <div className="aside">
+          <h3>About The Developer</h3>
+          <p>
+            Dolor laboris tempor ullamco nostrud proident aliquip sit cupidatat dolor. Ullamco nulla aliqua nostrud do
+            qui laborum adipisicing exercitation id cillum tempor dolore amet. In aliqua ad nisi do est in minim anim ex
+            dolore aliqua ipsum. Nulla adipisicing
+          </p>
+          <Contact />
         </div>
       </div>
     )
