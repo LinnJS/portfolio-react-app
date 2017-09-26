a38d907ed19c308d55a78afa67e6b273f095ae65
JSLinn727 <justinslinn727@gmail.com>
2017-09-26 09:42:45 -0400

    Added tooltips to icons

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './Skills.css'
+import ReactTooltip from 'react-tooltip'
 
 // Pro
 import htmlHex from '../../assets/hexIcons/htmlHex.png'
@@ -29,36 +30,64 @@ class Skills extends Component {
     return (
       <div className="skillsWrapper">
         <h3>Skills</h3>
-
         <div className="proRow1 skillCard">
           <h4>Proficient</h4>
-          <img className="hex" src={htmlHex} alt="HTML5" />
-          <img className="hex" src={cssHex} alt="CSS3" />
-          <img className="hex" src={jsHex} alt="JavaScript" />
-          <img className="hex" src={reactHex} alt="React" />
-          <img className="hex" src={gitHex} alt="Git" />
+          <img className="hex" src={htmlHex} alt="HTML5" data-place="bottom" data-effect="solid" data-tip="HTML5" />
+          <ReactTooltip />
+          <img className="hex" src={cssHex} alt="CSS3" data-place="bottom" data-effect="solid" data-tip="CSS3" />
+          <img
+            className="hex"
+            src={jsHex}
+            alt="JavaScript"
+            data-place="bottom"
+            data-effect="solid"
+            data-tip="JavaScript"
+          />
+          <img className="hex" src={reactHex} alt="React" data-place="bottom" data-effect="solid" data-tip="React" />
+          <img className="hex" src={gitHex} alt="Git" data-place="bottom" data-effect="solid" data-tip="Git" />
         </div>
 
         <div className="midRow1 skillCard">
           <h4>Intermediate</h4>
-          <img className="hex" src={jqueryHex} alt="jQuery" />
-          <img className="hex" src={nodeHex} alt="NodeJS" />
-          <img className="hex" src={expressHex} alt="ExpressJS" />
+          <img className="hex" src={jqueryHex} alt="jQuery" data-place="bottom" data-effect="solid" data-tip="jQuery" />
+          <img className="hex" src={nodeHex} alt="NodeJS" data-place="bottom" data-effect="solid" data-tip="NodeJS" />
+          <img
+            className="hex"
+            src={expressHex}
+            alt="ExpressJS"
+            data-place="bottom"
+            data-effect="solid"
+            data-tip="ExpressJS"
+          />
         </div>
 
         <div className="novRow1 skillCard">
           <h4>Novice</h4>
-          <img className="hex" src={reduxHex} alt="Redux" />
-          <img className="hex" src={jestHex} alt="Jest" />
-          <img className="hex" src={webpackHex} alt="Webpack" />
+          <img className="hex" src={reduxHex} alt="Redux" data-place="bottom" data-effect="solid" data-tip="Redux" />
+          <img className="hex" src={jestHex} alt="Jest" data-place="bottom" data-effect="solid" data-tip="Jest" />
+          <img
+            className="hex"
+            src={webpackHex}
+            alt="Webpack"
+            data-place="bottom"
+            data-effect="solid"
+            data-tip="Webpack"
+          />
         </div>
 
         <div className="learnRow1 skillCard">
           <h4>Learning Next</h4>
-          <img className="hex" src={meteorHex} alt="MeteorJS" />
-          <img className="hex" src={sketchHex} alt="Sketch" />
-          <img className="hex" src={sassHex} alt="Sass" />
-          <img className="hex" src={swiftHex} alt="Swift" />
+          <img
+            className="hex"
+            src={meteorHex}
+            alt="MeteorJS"
+            data-place="bottom"
+            data-effect="solid"
+            data-tip="MeteorJS"
+          />
+          <img className="hex" src={sketchHex} alt="Sketch" data-place="bottom" data-effect="solid" data-tip="Sketch" />
+          <img className="hex" src={sassHex} alt="Sass" data-place="bottom" data-effect="solid" data-tip="Sass" />
+          <img className="hex" src={swiftHex} alt="Swift" data-place="bottom" data-effect="solid" data-tip="Swift" />
         </div>
       </div>
     )
