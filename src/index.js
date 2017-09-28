e02ab8d8843a229b066ff08af50bdd2dd7c40ddd
JSLinn727 <justinslinn727@gmail.com>
2017-09-28 16:01:38 -0400

    remove sw

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App/App'
-import registerServiceWorker from './registerServiceWorker'
 
 const render = () => {
   ReactDOM.render(<App />, document.getElementById('root'))
 }
 
 render()
-registerServiceWorker()
 
 if (module.hot) {
   module.hot.accept('./components/App/App', render)
