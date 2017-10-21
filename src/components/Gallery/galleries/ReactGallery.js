15b5b4bcd7a91949fe67046d8877b9b9dde5332f
JSLinn727 <justinslinn727@gmail.com>
2017-10-21 18:35:41 -0400

    Added intelliGuide demo day project

--- src/components/Gallery/galleries/ReactGallery.js
+++ src/components/Gallery/galleries/ReactGallery.js
@@ -1,22 +1,31 @@
-import React, { PropTypes } from 'react'
-import './galleries.css'
+import React, { PropTypes } from 'react';
+import './galleries.css';
 
-import swicon from './icons/swicon.png'
+import swicon from './icons/swicon.png';
+import dali from './icons/dali.png';
 
 export default class ReactGallery extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
-      <div className="item">
-        <a href="https://jslinn727.github.io/starwarsReactApp/" target="_blank">
-          <img className="itemImg" src={swicon} alt="Star Wars Vehicles" />
-        </a>
-        <h5>Star Wars Vehicles Encyclopedia</h5>
-        <p>This app uses React to pull data from the SWAPI and displays stats on all vehicles.</p>
+      <div className="ReactGallery">
+        <div className="item">
+          <a href="https://jslinn727.github.io/intelli-guide/" target="_blank">
+            <img className="itemImg" src={dali} alt="Star Wars Vehicles" />
+          </a>
+          <h5>IntelliGuide</h5>
+          <p>
+            Museum audioguide platform branded for The Dali Museum. Made with React front-end, Node screen scraper, and
+            FireBaseDB.
+          </p>
+        </div>
+        <div className="item">
+          <a href="https://jslinn727.github.io/starwarsReactApp/" target="_blank">
+            <img className="itemImg" src={swicon} alt="Star Wars Vehicles" />
+          </a>
+          <h5>Star Wars Vehicles Encyclopedia</h5>
+          <p>This app uses React to pull data from the SWAPI and displays stats on all vehicles.</p>
+        </div>
       </div>
-    )
+    );
   }
 }
