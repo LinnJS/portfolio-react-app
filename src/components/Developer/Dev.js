39f571f61e7521c374a56186e35424994c879283
JSLinn727 <justinslinn727@gmail.com>
2017-11-08 12:15:40 -0500

    Fixedm minor bugs

--- src/components/Developer/Dev.js
+++ src/components/Developer/Dev.js
@@ -6,16 +6,16 @@ import Contact from '../Contact/Contact';
 class Dev extends Component {
   render() {
     return (
-      <div className="card" id="devCar">
+      <div className="card" id="devCard">
         <img className="headshot" src={headshot} alt=" The Developer" />
         <div className="aside">
           <h3>About The Developer</h3>
-          <p className="text">
+          <p>
             Justin is an entrepreneurial-minded young buck eager to make his mark in the technology industry. He thrives
             under pressure and is relentless in the pursuit of creative problem-solving. Justin is a goal-oriented
             value-driven developer who loves to make life easier, so we all can focus on the important things in life.
           </p>
-          <p className="text">
+          <p>
             Justin's favorite aspect of software development is making an intuitive UI that makes sense, and a seamless
             UX that makes users excited to use technology. He is also very adaptive and can complete many tasks on the
             backend to get the job done using popular frameworks. If you can think it Justin can learn it, and deliver
